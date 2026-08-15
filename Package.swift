1bfabe94268f6260930712431751f2d703b726da
Steffan Andrews <stef@orchetect.com>
2026-08-15 01:05:56 -0700

    Bumped SwiftTimecode dependency to 3.1.4

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/orchetect/swift-data-parsing", from: "0.1.2"),
         .package(url: "https://github.com/orchetect/swift-extensions", from: "3.0.0"),
         .package(url: "https://github.com/orchetect/swift-radix", from: "1.4.1"),
-        .package(url: "https://github.com/orchetect/swift-timecode", from: "3.1.3")
+        .package(url: "https://github.com/orchetect/swift-timecode", from: "3.1.4")
     ],
     targets: [
         .target(
